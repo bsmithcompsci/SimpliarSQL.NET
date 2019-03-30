8bae5b398cc99f734f1747d8b486fab23aae6443
xrayhunter <3002612+xrayhunter@users.noreply.github.com>
2019-03-30 16:22:22 -0700

    V1.0.2.1 Added Encryption to the library

---
+++ SimpliarSQL.NET/Core/Encryption/Hash.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace SimpliarSQL.Core.Encryption
+{
+    public class Hash
+    {
+        public static string Sha256(string raw)
+        {
+            using (SHA256 hash = SHA256.Create())
+            {
+                byte[] bytes = hash.ComputeHash(Encoding.UTF8.GetBytes(raw));
+
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                    builder.Append(bytes[i].ToString("x2"));
+
+                // Memory clean...
+                hash.Dispose();
+
+                return builder.ToString();
+            }
+        }
+
+        public static string GenerateSeed(uint range = 32)
+        {
+            using (RandomNumberGenerator rng = new RNGCryptoServiceProvider())
+            {
+                byte[] tokenData = new byte[range];
+                rng.GetBytes(tokenData);
+
+                // Memory clean...
+                rng.Dispose();
+                return Convert.ToBase64String(tokenData);
+            }
+        }
+    }
+}
